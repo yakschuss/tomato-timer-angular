9acbba20c5fbb3cb44474a33acf7c6f423373940
Jack Schuss <jackschuss@gmail.com>
2016-07-31 16:05:19 -0400

    almost done, getting sound to work

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,10 +1,23 @@
 (function () {
-  function MainCtrl(Timer) {
+  function MainCtrl($scope, Timer) {
+
     this.timer = Timer;
+    $scope.timer = Timer;
+
+    var myDing = new buzz.sound( "/assets/sounds/ding.mp3", {
+      preload: true
+    });
+
+    $scope.$watch('timer.time', function(newVal, o) {
+      if(newVal == 0) {
+        myDing.play();
+      }
+    });
   }
+
 
   angular
     .module('tomatoTimer')
-    .controller('MainCtrl', ['Timer', MainCtrl ]);
-  
+    .controller('MainCtrl', [ '$scope', 'Timer', MainCtrl ]);
+
 })();
--- app/scripts/services/Timer.js
+++ app/scripts/services/Timer.js
@@ -1,14 +1,82 @@
 (function () {
-  function Timer($rootScope) {
+  function Timer($interval) {
 
     var Timer = {};
 
+    Timer.time = 2;
+    Timer.btnTxt = "Start";
+    Timer.completedSessions = 0;
+
+
+    Timer.start = function() {
+
+      Timer.isRunning = true;
+      Timer.btnTxt = "Reset";
+
+      Timer.timer = $interval(function (){
+        Timer.time -= 1;
+
+        if(Timer.time == -1 && Timer.completedSessions == 3) {
+          $interval.cancel(Timer.timer);
+          Timer.isRunning = false;
+          Timer.time = 15;
+          Timer.completedSessions = 0;
+          Timer.onBreak = true;
+          Timer.btnTxt = "Take A Break";
+        }
+
+        else if(Timer.time == -1) {
+          $interval.cancel(Timer.timer);
+          Timer.isRunning = false;
+          Timer.time = 2;
+          Timer.completedSessions += 1;
+          Timer.btnTxt = "Next Session";
+        }
+
+
+      }.bind(this), 1000);
+    }
+
+
+
+
+    Timer.reset = function() {
+      if (angular.isDefined(Timer.timer)) {
+        $interval.cancel(Timer.timer);
+        Timer.isRunning = false;
+        Timer.time = 2;
+        Timer.btnTxt = "Start Working";
+
+      }
+    }
+
+    Timer.break = function() {
+      Timer.isRunning = true;
+
+      Timer.onBreak = false;
+      Timer.breakMsg = "Start Break"
+
+      Timer.btnTxt = "Reset Timer";
+
+      Timer.timer = $interval(function () {
+
+        Timer.time -= 1;
+        if(Timer.time == -1) {
+          $interval.cancel(Timer.timer);
+          Timer.isRunning = false;
+          Timer.btnTxt = "Nice Work! Start Again!"
+          Timer.time = 2;
+        }
+      }.bind(this), 1000);
+    }
+
     return Timer
+
   }
 
   angular
     .module('tomatoTimer')
-    .factory('Timer', ['$rootScope', Timer ]);
+    .factory('Timer', ['$interval', Timer ]);
 
 
 })();
